03439d2f75a340b5db2b013ffe0524f7abf07f21
Jack <jackbroomey.01@gmail.com>
2020-06-01 16:37:20 +0100

    First

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,20 @@
+#include "Application.h"
+
+namespace Hazel {
+	Application::Application()
+	{
+	}
+
+	Application::~Application()
+	{
+	}
+
+	// Main loop
+	void Application::Run()
+	{
+
+		while (true);
+	}
+
+	
+}
